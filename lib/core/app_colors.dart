a92b3846d30cea759c07992c5b8e9babc30ce01d
DanielRTato <drodrigueztato@danielcastelao.org>
2026-03-02 19:13:59 +0100

    Colores de la aplicación

--- lib/core/app_colors.dart
+++ lib/core/app_colors.dart
@@ -1,3 +1,14 @@
+import 'dart:ui';
+
 class AppColors {
-  
+  static const Color primaryColor = Color(0xFF6200EA);
+    static const Color secondaryColor = Color(0xFF2A0464);
+      static const Color accentColor = Color(0xFFE9F547);
+
+
+  // Background colors
+  static const Color backgroundColor = Color(0xFF0E0B20);
+  static const Color backgroundComponent = Color(0xFF1D1E33);
+    static const Color backgroundComponentSelected = Color(0xFF4F548B);
+
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:imc_calculator/core/app_colors.dart';
 
 void main() {
   runApp(const MainApp());
@@ -11,6 +12,7 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       home: Scaffold(
+        backgroundColor: AppColors.backgroundColor,
         body: Center(
           child: Text('Hello World!'),
         ),
