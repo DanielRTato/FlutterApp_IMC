87911bdfbeb6bc2dc9b8052798201b52cc647ac3
DanielRTato <drodrigueztato@danielcastelao.org>
2026-03-03 20:58:59 +0100

    gender selector

--- lib/components/gender_selector.dart
+++ lib/components/gender_selector.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:imc_calculator/core/app_colors.dart';
+import 'package:imc_calculator/core/text_styles.dart';
 
 class GenderSelector extends StatefulWidget {
   const GenderSelector({super.key});
@@ -12,6 +14,34 @@ class _GenderSelectorState extends State<GenderSelector> {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Row(children: [
+      Container(
+        decoration: BoxDecoration(
+          color: selectedGender == "HOMBRE" ? AppColors.backgroundComponentSelected : AppColors.backgroundComponent,
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Column(
+          children: [
+            Image.asset('assets/images/male.png', width: 100, height: 100),
+            Text("HOMBRE", style: TextStyles.bodyTextStyle),
+            ],
+        ),
+      ),
+      Container(
+                decoration: BoxDecoration(
+          color: selectedGender == "MUJER" ? AppColors.backgroundComponentSelected : AppColors.backgroundComponent,
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Column(
+          children: [
+            Image.asset('assets/images/female.png', width: 100, height: 100),
+            Text("Mujer".toUpperCase(), style: TextStyles.bodyTextStyle),
+        
+          ],  
+          ),
+      ),
+      ]
+    );
+    
   }
 }
---
+++ lib/core/text_styles.dart
@@ -0,0 +1,8 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class TextStyles {
+  
+  static const TextStyle bodyTextStyle = TextStyle(color: Colors.white, fontSize: 18, fontWeight: FontWeight.bold);
+}
