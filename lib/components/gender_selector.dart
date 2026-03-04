472a15c5404d534a15ff022e3154953e87769722
DanielRTato <drodrigueztato@danielcastelao.org>
2026-03-04 17:54:49 +0100

    gender selector

--- lib/components/gender_selector.dart
+++ lib/components/gender_selector.dart
@@ -15,30 +15,60 @@ class _GenderSelectorState extends State<GenderSelector> {
   @override
   Widget build(BuildContext context) {
     return Row(children: [
-      Container(
-        decoration: BoxDecoration(
-          color: selectedGender == "HOMBRE" ? AppColors.backgroundComponentSelected : AppColors.backgroundComponent,
-          borderRadius: BorderRadius.circular(10),
-        ),
-        child: Column(
-          children: [
-            Image.asset('assets/images/male.png', width: 100, height: 100),
-            Text("HOMBRE", style: TextStyles.bodyTextStyle),
-            ],
+      Expanded(
+        child: GestureDetector(
+             onTap: () {
+            setState(() {
+              selectedGender = "HOMBRE";
+            });
+          },
+          child: Padding(
+            padding: const EdgeInsets.only(left: 16, right: 8, top: 16, bottom: 16),
+            child: Container(
+              decoration: BoxDecoration(
+                color: selectedGender == "HOMBRE" ? AppColors.backgroundComponentSelected : AppColors.backgroundComponent,
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(12),
+                child: Column(
+                  children: [
+                    Image.asset('assets/images/male.png', width: 100, height: 100),
+                    Text("HOMBRE", style: TextStyles.bodyTextStyle),
+                    ],
+                ),
+              ),
+            ),
+          ),
         ),
       ),
-      Container(
-                decoration: BoxDecoration(
-          color: selectedGender == "MUJER" ? AppColors.backgroundComponentSelected : AppColors.backgroundComponent,
-          borderRadius: BorderRadius.circular(10),
-        ),
-        child: Column(
-          children: [
-            Image.asset('assets/images/female.png', width: 100, height: 100),
-            Text("Mujer".toUpperCase(), style: TextStyles.bodyTextStyle),
-        
-          ],  
+      Expanded(
+        child: GestureDetector(
+          onTap: () {
+            setState(() {
+              selectedGender = "MUJER";
+            });
+          },
+          child: Padding(
+            padding: const EdgeInsets.only(left: 8, right: 16, top: 16, bottom: 16),
+            child: Container(
+                      decoration: BoxDecoration(
+                color: selectedGender == "MUJER" ? AppColors.backgroundComponentSelected : AppColors.backgroundComponent,
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(12),
+                child: Column(
+                  children: [
+                    Image.asset('assets/images/female.png', width: 100, height: 100),
+                    Text("Mujer".toUpperCase(), style: TextStyles.bodyTextStyle),
+                
+                  ],  
+                  ),
+              ),
+            ),
           ),
+        ),
       ),
       ]
     );
